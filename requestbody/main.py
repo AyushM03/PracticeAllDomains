44b5e79bbcb744da78f408da40c97a6455431034
AyushM03 <aayushmeshram9168@gmail.com>
2026-04-06 00:48:06 +0530

    pydantic aftervalidator learning for custom validation

--- requestbody/main.py
+++ requestbody/main.py
@@ -48,15 +48,55 @@
 
 #Code
 
-from typing import Annotated
+#  from typing import Annotated
+#  from fastapi import FastAPI, Query
+
+# app=FastAPI()
+
+# @app.get("/items/")
+# async def read_items(q:Annotated[str | None, Query(max_length=50)]=None):
+#     results={"items":[{"items_id":"Foo","owner":"Bar"}]}
+#     if q:
+#         results.update({"q": q})
+#     return results
+
+#multivalues using query parameters and Query decorator and annotated types
+
+#Code
+
+# @app.get("/items/")
+# async def read_items(q: Annotated[list | None, Query()]=["Foo", "Bar"]):
+#     results={"items":[{"items_id":"Foo","owner":"Bar"}]}
+#     if q:
+#         results.update({"q": q})
+#     return results
+
+#---------------------------------------------------------
+
+#custom validations using pydantic afterValidator decorator inside annotated types
+
 from fastapi import FastAPI, Query
+from typing import Annotated
+from pydantic import AfterValidator
+
 
 app=FastAPI()
 
-@app.get("/items/")
-async def read_items(q:Annotated[str | None, Query(max_length=50)]=None):
-    results={"items":[{"items_id":"Foo","owner":"Bar"}]}
-    if q:
-        results.update({"q": q})
-    return results
+data = {
+    "isbn-9781529046137": "The Hitchhiker's Guide to the Galaxy",
+    "imdb-tt0371724": "The Hitchhiker's Guide to the Galaxy",
+    "isbn-9781439512982": "Isaac Asimov: The Complete Stories, Vol. 2",
+}
 
+def check_values(id:str):
+    if not id.startswith(("isbn-", "imdb-")):
+        raise ValueError("id invalid value")
+    return id
+
+@app.get("/items/")
+async def read_items(id:Annotated[str | None, AfterValidator(check_values)]=None):
+    if id:
+        item=data.get(id)
+    else:
+        id,item=random.choice(list(data.items()))
+    return {"id": id, "item": item}
