e3067bc44425e0edf2a5a7b53661b679cce58b4d
AyushM03 <aayushmeshram9168@gmail.com>
2026-04-06 11:45:11 +0530

    learnt about genai image generation using dotnet

---
+++ genaiimgdotnet/demo01/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+// this will generate the images just put the api key and run dotnet run in terminal.
+using  OpenAI_API;
+
+var api = new OpenAIAPI("OPENAPIKEY");
+
+var request= new OpenAI_API.Images.ImageGenerationRequest
+{
+    Prompt = "a white siamese cat",
+};
+
+var response = await api.ImageGenerations.CreateImageAsync(request);
+Console.WriteLine("Image URL: {0}",response.Data[0].Url);
