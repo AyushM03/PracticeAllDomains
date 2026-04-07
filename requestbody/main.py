6b2383aa98708b9f99641f3e3c9d065ad9f5da9a
AyushM03 <aayushmeshram9168@gmail.com>
2026-04-07 20:21:10 +0530

    validation using path. query use for query parameter and path use for path parameter

--- requestbody/main.py
+++ requestbody/main.py
@@ -75,28 +75,48 @@
 
 #custom validations using pydantic afterValidator decorator inside annotated types
 
-from fastapi import FastAPI, Query
-from typing import Annotated
-from pydantic import AfterValidator
+# from fastapi import FastAPI, Query
+# from typing import Annotated
+# from pydantic import AfterValidator
 
+
+# app=FastAPI()
+
+# data = {
+#     "isbn-9781529046137": "The Hitchhiker's Guide to the Galaxy",
+#     "imdb-tt0371724": "The Hitchhiker's Guide to the Galaxy",
+#     "isbn-9781439512982": "Isaac Asimov: The Complete Stories, Vol. 2",
+# }
+
+# def check_values(id:str):
+#     if not id.startswith(("isbn-", "imdb-")):
+#         raise ValueError("id invalid value")
+#     return id
+
+# @app.get("/items/")
+# async def read_items(id:Annotated[str | None, AfterValidator(check_values)]=None):
+#     if id:
+#         item=data.get(id)
+#     else:
+#         id,item=random.choice(list(data.items()))
+#     return {"id": id, "item": item}
+
+
+#------------------------------------------------------------
+
+#you can declare the same type of validations and metadata for path parameters with Path.
+
+from typing import Annotated
+from fastapi import FastAPI, Query, Path
 
 app=FastAPI()
 
-data = {
-    "isbn-9781529046137": "The Hitchhiker's Guide to the Galaxy",
-    "imdb-tt0371724": "The Hitchhiker's Guide to the Galaxy",
-    "isbn-9781439512982": "Isaac Asimov: The Complete Stories, Vol. 2",
-}
-
-def check_values(id:str):
-    if not id.startswith(("isbn-", "imdb-")):
-        raise ValueError("id invalid value")
-    return id
-
-@app.get("/items/")
-async def read_items(id:Annotated[str | None, AfterValidator(check_values)]=None):
-    if id:
-        item=data.get(id)
-    else:
-        id,item=random.choice(list(data.items()))
-    return {"id": id, "item": item}
+@app.get("/items/{item_id}")
+async def read_items(
+    item_id:Annotated[int,Path(title="The ID of the item ")],
+    q: Annotated[str | None, Query(alias="item-query")] = None,
+):
+    results = {"item_id": item_id}
+    if q:
+        results.update({"q": q})
+    return results
