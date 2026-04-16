33938ffabf7eaa19c3ccd05638751c286cf4d5f9
AyushM03 <aayushmeshram9168@gmail.com>
2026-04-16 21:21:58 +0530

    learning about query parameter model with pydantic

--- requestbody/main.py
+++ requestbody/main.py
@@ -122,7 +122,19 @@
 #     return results
 
 #-------------------------------------------------------------------
-from typing import Annotated
+from typing import Annotated, Literal
 from fastapi import FastAPI, Query, Path
-
+from pydantic import BaseModel, Field
 app=FastAPI()
+
+class FilterParams(BaseModel):
+    limit: int = Field(100, ge=0,le=100)
+    offset: int = Field(0, ge=0)
+    order_by: Literal["created_at", "updated_at"] = "created_at"
+    tags: list[str] = []
+
+@app.get("/items/")
+async def read_items(
+    filters_query: Annotated[FilterParams, Query()]
+):
+    return filters_query
