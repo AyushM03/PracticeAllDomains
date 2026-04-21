bba9409f3af2f06c532ce3833e60330cc28e8636
AyushM03 <aayushmeshram9168@gmail.com>
2026-04-21 20:47:01 +0530

    revised all about fastAPI

--- genaiimgdotnet/demo01/obj/Debug/net9.0/demo01.AssemblyInfo.cs
+++ genaiimgdotnet/demo01/obj/Debug/net9.0/demo01.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("demo01")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+33938ffabf7eaa19c3ccd05638751c286cf4d5f9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+77be718d0f614f5ea40b49aade15e60f738e7371")]
 [assembly: System.Reflection.AssemblyProductAttribute("demo01")]
 [assembly: System.Reflection.AssemblyTitleAttribute("demo01")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- requestbody/main.py
+++ requestbody/main.py
@@ -218,3 +218,8 @@ class Item(BaseModel):
 async def update_item(item_id: int, item: Annotated[Item, Body(embed=True)]):
     results = {"item_id": item_id, "item": item}
     return results
+
+
+
+
+# today i revised all things from scratch.
